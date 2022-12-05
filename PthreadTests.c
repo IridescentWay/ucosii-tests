2c4e9a9ae9e350b088e517aa796985c20555baaa
VereLee <verelee@foxmail.com>
2022-12-05 16:55:03 +0800

    2022/12/5 ljh: remove cpputest

---
+++ PthreadTests.c
@@ -0,0 +1,22 @@
+﻿#include "face_pthread.h"
+
+void task1_func()
+{
+	printf("executing task1 ......\n");
+	while (1) {
+		printf("task1 still running ......\n");
+		sleep(1);
+	}
+	
+}
+
+void pthread_test_normaloperation()
+{
+	pthread_t* thread = (pthread_t*) malloc(sizeof(pthread_t));
+	pthread_create(thread, (const pthread_attr_t*) NULL, task1_func, NULL);
+	int res = pthread_detach(thread);
+	if (res != 0) {
+		printf("__FILE__:%s\n", __FILE__);
+		printf("__LINE__:%d\n", __LINE__ - 3);
+	}
+}
---
+++ include/PthreadTests.h
@@ -0,0 +1,6 @@
+#ifndef __PTHREAD_TESTS_H__
+#define __PTHREAD_TESTS_H__
+
+void pthread_test_normaloperation();
+
+#endif
